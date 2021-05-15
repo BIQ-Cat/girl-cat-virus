032ff5756add07853758d300985f5b9926481d6d
Catgirllover <84183255+Catgirllover@users.noreply.github.com>
2021-05-15 12:44:11 +0300

    Add files via upload

---
+++ __DIRTY__/show3.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtGui import QPixmap
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('q(≧▽≦q)')
+        self.setGeometry(200, 200, 1, 1)
+
+    def load_image(self, file_name):
+        pixmap = QPixmap(file_name)
+
+        self.label = QLabel(self)
+        self.label.setPixmap(pixmap)
+        self.label.resize(pixmap.width(), pixmap.height())
+
+        self.resize(pixmap.width(), pixmap.height())
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+
+    ex = App()
+    ex.load_image('..\__MATERIALS__\cat3.gif')
+    ex.show()
+
+    sys.exit(app.exec_())
